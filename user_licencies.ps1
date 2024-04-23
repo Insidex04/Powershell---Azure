e1dd9ac59f37d6e92226a6abdaefd2ad49d141c6
Insidex04 <127747853+Insidex04@users.noreply.github.com>
2024-04-23 21:20:55 +0200

    Create user_licencies.ps1

---
+++ user_licencies.ps1
@@ -0,0 +1,11 @@
+# Obtener todos los usuarios
+$usuarios = Get-MgUser -All | Select-Object -Property DisplayName,UserPrincipalName
+
+# Iterar a través de cada usuario
+foreach ($usuario in $usuarios) {
+    # Obtener los detalles de la licencia para el usuario actual
+    $detallesLicencia = Get-MgUserLicenseDetail -UserId $usuario.UserPrincipalName
+    
+    # Seleccionar Nombre, mail y tipo de licencia.
+    $detallesLicencia | Select-Object @{Name="DisplayName"; Expression={$usuario.DisplayName}}, @{Name="UserPrincipalName"; Expression={$usuario.UserPrincipalName}}, SkuPartNumber
+}
